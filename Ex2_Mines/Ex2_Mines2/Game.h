1c0aadb5c86499cd07794408b980b57f80d78c21
n86-64 <nathan.butt@gmail.com>
2017-02-05 18:17:26 +0000

    Added a .gitignore file and the Entrypoint for the Application as well as the header for the Game class.

---
+++ Ex2_Mines/Ex2_Mines2/Game.h
@@ -0,0 +1,36 @@
+/*
+Module - UFCFWA-30-1
+Name - Nathan Butt
+Student No - 16013327
+
+Exersice 2 - Minesweeper
+File Name - Game.h
+File Contents - The class definition for the Game Class. 
+
+Purpose - The main class object which will process all the game logic. 
+*/
+
+
+#pragma once
+
+
+#include <iostream>
+#include <string>
+#include <time.h>
+
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void startGame();
+
+private: 
+	void startGameLoop();
+	void checkInput(int numberOfParams);
+	void Quit(); 
+
+private:
+
+};
---
+++ Ex2_Mines/Ex2_Mines2/Main.cpp
@@ -0,0 +1,17 @@
+/*
+Module - UFCFWA-30-1
+Name - Nathan Butt
+Student No - 16013327
+
+Exersice 2 - Minesweeper
+File Name - Main.cpp
+File Contents - The entrypoint to the Minesweeper Game. 
+
+Purpose - The main thread where the application will both start and terminate. 
+*/
+
+
+int main() 
+{
+   
+}
